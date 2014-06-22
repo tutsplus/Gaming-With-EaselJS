ad237ac0347b2f2c3185486e30c8906e86668fe1
Jason Green <jasoniangreen@gmail.com>
2014-06-22 12:32:42 +0100

    First commit. Grunt, npm, bower all setup with grunt tasks.

---
+++ app/lib/config.js
@@ -0,0 +1,5 @@
+'use strict';
+
+module.exports = {
+    name: 'Advanced Canvas Tutorials'
+};
---
+++ app/lib/main.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var config = require('./config')
+    , c = createjs;
+
+console.log('Game Started: EaselJS version: ' + c.EaselJS.version);
+
+window.onload = function onLoad() {
+    var stage = new c.Stage("main");
+    var circle = new c.Shape();
+    circle.graphics.beginFill("red").drawCircle(0, 0, 50);
+    circle.x = 100;
+    circle.y = 100;
+    stage.addChild(circle);
+    stage.update();
+}
