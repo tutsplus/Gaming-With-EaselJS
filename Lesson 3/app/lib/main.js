71d9e1ed465674047eb5ce9a65718496d7767fd4
Jason Green <jasoniangreen@gmail.com>
2014-06-28 11:18:36 +0100

    Starting on lesson 3

--- Lesson 3/app/lib/main.js
+++ Lesson 3/app/lib/main.js
@@ -1,15 +1,21 @@
 'use strict';
 
+var domReady = require('./util').domReady;
+
 var c = createjs;
 
 console.log('Game Started: EaselJS version: ' + c.EaselJS.version);
 
-window.onload = function onLoad() {
+
+domReady(function init() {
     var stage = new c.Stage("main");
     var circle = new c.Shape();
-    circle.graphics.beginFill("red").drawCircle(0, 0, 50);
+    circle.graphics.beginFill("green").drawCircle(0, 0, 50);
     circle.x = 100;
     circle.y = 100;
     stage.addChild(circle);
     stage.update();
-}
+
+
+    //shape, bitmap and display object
+});
---
+++ Lesson 3/app/lib/util/index.js
@@ -0,0 +1,5 @@
+'use strict';
+
+module.exports = {
+    domReady: require('./dom_ready')
+};
